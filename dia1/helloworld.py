2e56ce370d4f330792ac9ef2d4e4db5cf2c5dbd4
AntonioTroncoso <atrobar0610@m.educaand.es>
2022-12-02 10:56:27 +0100

    dia1

--- dia1/helloworld.py
+++ dia1/helloworld.py
@@ -1,4 +1,4 @@
-level 1/
+#level 1 
 print (3+4)
 print (3-4)
 print (3*4)
@@ -14,17 +14,17 @@ print(type(3.14))
 print(type(9.8))
 print(type(4-4j))
 print(type("aaron""cantador gomez""españa"))
-level/3/
-print(type(3-3)
+# level 3
+print(type(3-3))
 print(type(5.5))
-print(type(5x-2)
-print("buenas tardes"))
+print(type(3*-5))
+print("buenas tardes")
 print(7==3)
 print["goma""boli""rotulador"]
 print("cristal", "mesa","silla", "pizarra")
-print{1, 2, 3, 4, 5}
-print {"nombre":"aaron",
+print({1, 2, 3, 4, 5})
+print({ "nombre":"aaron",
 "apellidos":"cantador gomez",
 "pais":"españa",
 "edad":"16 años"
-}
+})

0dd6bf228411c70b745b0b9e2e92a9061ac77095
AntonioTroncoso <atrobar0610@m.educaand.es>
2022-12-02 10:45:55 +0100

    dia1

---
+++ dia1/Day_4/strings.py
@@ -0,0 +1,84 @@
+
+print('Thirty' + ' Days' + ' Of' + ' Python')
+
+
+print(' '.join(['Coding', 'For', 'All']))
+
+
+company = 'Coding For All'
+
+
+print(company)
+
+
+print(len(company))
+
+
+print(company.upper())
+
+
+print(company.lower())
+
+
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+
+print(company[7:])
+
+
+print(company[company.index('Coding'):8])
+print(company[company.find('Coding'):8])
+
+
+print(company.replace('Coding ', 'Not '))
+
+
+print('Python For Everyone'.replace('Everyone', 'All'))
+
+
+print(company.split())
+
+
+print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(','))
+
+
+print(company[0])
+
+
+print(len(company) - 1)
+
+
+print(company[10])
+
+words = "Python For Everyone".split()
+print(words[0][0] + words[1][0] + words[2][0])
+
+
+words = 'Coding For All'.split()
+print(words[0][0] + words[1][0] + words[2][0])
+
+
+print("Coding For All".index('C'))
+
+
+print("Coding For All".index('F'))
+
+
+print("Coding For All People".rfind('l'))
+
+
+print('You cannot end a sentence with because because because is a conjunction'.find('because'))
+
+
+print('You cannot end a sentence with because because because is a conjunction'.rfind('because'))
+
+
+print('You cannot end a sentence with because because because is a conjunction'.replace('because because because', ''))
+
+
+print('You cannot end a sentence with because because because is a conjunction'.find('because'))
+
+
+print('You cannot end a sentence with because because because is a conjunction'.replace('because because because', ''))
---
+++ dia1/helloworld.py
@@ -0,0 +1,30 @@
+level 1/
+print (3+4)
+print (3-4)
+print (3*4)
+print (3%4)
+print (3/4)
+print (3**4)
+print (3//4)
+print ("aaron")
+print ("cantador gomez")
+print ("españa")
+print(type(10))
+print(type(3.14))
+print(type(9.8))
+print(type(4-4j))
+print(type("aaron""cantador gomez""españa"))
+level/3/
+print(type(3-3)
+print(type(5.5))
+print(type(5x-2)
+print("buenas tardes"))
+print(7==3)
+print["goma""boli""rotulador"]
+print("cristal", "mesa","silla", "pizarra")
+print{1, 2, 3, 4, 5}
+print {"nombre":"aaron",
+"apellidos":"cantador gomez",
+"pais":"españa",
+"edad":"16 años"
+}
